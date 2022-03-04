b8981ecc211a1768363254bae4e3b08539c67090
dc25dc52 <85923022+dc25dc52@users.noreply.github.com>
2022-03-04 16:16:02 +0800

    01

--- app/src/main/java/com/tom/bmi2/MainActivity.kt
+++ app/src/main/java/com/tom/bmi2/MainActivity.kt
@@ -10,16 +10,21 @@ import com.tom.bmi2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
     fun btn(view:View){
         var weight = binding.edWeight.text.toString().toFloat()
         var height = binding.edHeight.text.toString().toFloat()
         var bmi= weight / (height*height).toFloat()
-        Toast.makeText(this,bmi.toString(),Toast.LENGTH_LONG).show()
+
+        binding.textView4.visibility = View.VISIBLE
+        binding.textView4.text="bmi= $bmi "
+//        Toast.makeText(this,bmi.toString(),Toast.LENGTH_LONG).show()
 
     }
 }
